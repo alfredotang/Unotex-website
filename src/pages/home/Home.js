f856ac2195bf2e53f09caefa64f08ab71edb5f2d
Alfredo <asdzxcca0617@gmail.com>
2018-07-11 22:13:19 +0800

    home-pages background-image

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import './home.css';
 import home from './home2.png';
 import Footer from './Footer';
 import Bottom from '../../components/Bottom';
 
 
-const sections={
-    backgroundImage:`url(${home})`,
-    backgroundRepeat:'no-repeat',
-    backgroundSize:'cover',
-    width: '100%',
-    height: '100vh',
-    
-}
+
+const Section = styled.section`
+    background-image:url(${home});
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+    @media(max-width:420px){
+        background-position:center;
+    }    
+`;
 
 
 class Home extends Component{
@@ -21,7 +25,7 @@ class Home extends Component{
         return(
                 
             <div>
-                <section className="sec1" style={sections}></section>
+                <Section></Section>
                 <footer>
                     <Footer />
                 </footer>
